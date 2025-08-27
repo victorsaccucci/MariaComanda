88ff09d9b9c4681289c2680360d56e7a5e794707
victorsaccucci <victorsaccucciv@gmail.com>
2025-08-26 21:15:17 -0300

    Primeiro commit

---
+++ src/main/java/com/fiap/mariacomanda/MariacomandaApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.mariacomanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MariacomandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MariacomandaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/mariacomanda/MariacomandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.mariacomanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MariacomandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
