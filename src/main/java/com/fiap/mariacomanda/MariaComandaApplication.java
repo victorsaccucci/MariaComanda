8a38f4e506281a3cf0f4c95a3dbcad1dfe5d20f0
ceciliafiap <cecilia-fiap.silva@hotmail.com>
2025-08-26 21:54:57 -0300

    Ajustado nome da aplicação, inserida refs de JPA, o arquivo docker-compose e configurado application.properties

--- src/main/java/com/fiap/mariacomanda/MariaComandaApplication.java
+++ src/main/java/com/fiap/mariacomanda/MariaComandaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MariacomandaApplication {
+public class MariaComandaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MariacomandaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MariaComandaApplication.class, args);
+    }
 
 }
