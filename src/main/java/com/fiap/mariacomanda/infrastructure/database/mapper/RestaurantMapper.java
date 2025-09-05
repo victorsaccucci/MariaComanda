cd217c0e62596c689db1d5cc3158fc5e22eafb01
ceciliafiap <cecilia-fiap.silva@hotmail.com>
2025-09-05 19:58:44 -0300

    Ajustado o mapper de core sem anotações e o de infra com component

--- src/main/java/com/fiap/mariacomanda/infrastructure/database/mapper/RestaurantMapper.java
+++ src/main/java/com/fiap/mariacomanda/infrastructure/database/mapper/RestaurantMapper.java
@@ -1,11 +1,17 @@
-package com.fiap.mariacomanda.core.mapper;
+package com.fiap.mariacomanda.infrastructure.database.mapper;
 
 import com.fiap.mariacomanda.core.domain.entity.Restaurant;
-import com.fiap.mariacomanda.core.dto.*;
+import com.fiap.mariacomanda.core.dto.CreateRestaurantInputDTO;
+import com.fiap.mariacomanda.core.dto.CreateRestaurantOutputDTO;
+import com.fiap.mariacomanda.core.dto.GetRestaurantOutputDTO;
+import com.fiap.mariacomanda.core.dto.UpdateRestaurantInputDTO;
 import com.fiap.mariacomanda.infrastructure.web.json.RestaurantJson;
+import org.springframework.stereotype.Component;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Component
 public class RestaurantMapper {
     public Restaurant mapCreate(CreateRestaurantInputDTO dto) {
         return new Restaurant(
