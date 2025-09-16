68c0e012cb8905e4093560e06d1111aedf4d5d91
victorsaccucci <victorsaccucciv@gmail.com>
2025-09-16 16:00:07 -0300

    feat implementando os usecases de usuário

--- src/main/java/com/fiap/mariacomanda/core/usecases/user/DeleteUserUseCase.java
+++ src/main/java/com/fiap/mariacomanda/core/usecases/user/DeleteUserUseCase.java
@@ -1,4 +1,7 @@
 package com.fiap.mariacomanda.core.usecases.user;
 
+import java.util.UUID;
+
 public interface DeleteUserUseCase {
+    void execute(UUID id);
 }
--- src/main/java/com/fiap/mariacomanda/core/usecases/user/DeleteUserUseCaseImpl.java
+++ src/main/java/com/fiap/mariacomanda/core/usecases/user/DeleteUserUseCaseImpl.java
@@ -1,4 +1,20 @@
 package com.fiap.mariacomanda.core.usecases.user;
 
-public class DeleteUserUseCaseImpl {
+import com.fiap.mariacomanda.core.gateway.UserGateway;
+
+import java.util.UUID;
+
+@
+public class DeleteUserUseCaseImpl implements DeleteUserUseCase{
+
+    private final UserGateway gateway;
+
+    public DeleteUserUseCaseImpl(UserGateway gateway) {
+        this.gateway = gateway;
+    }
+
+    @Override
+    public void execute(UUID id) {
+         gateway.deleteById(id);
+    }
 }
