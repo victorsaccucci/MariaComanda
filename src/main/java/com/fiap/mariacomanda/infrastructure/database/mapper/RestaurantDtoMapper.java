8a1f84283994d76169adf932393dbef1fffe906b
Caroline Barbara <caroline.sbarbara@gmail.com>
2025-09-16 20:46:33 -0300

    ajuste no nome dos mappers da infra

--- src/main/java/com/fiap/mariacomanda/infrastructure/database/mapper/RestaurantDtoMapper.java
+++ src/main/java/com/fiap/mariacomanda/infrastructure/database/mapper/RestaurantDtoMapper.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
-public class RestaurantMapper {
+public class RestaurantDtoMapper {
     public Restaurant mapCreate(CreateRestaurantInputDTO dto) {
         return new Restaurant(
             dto.getId(), 
